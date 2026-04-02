bbabf59f2175c100ce455a8e4539537ab50c285e
Wahid <wahidul.islam.3666@gmail.com>
2026-04-02 14:53:07 +0900

    Add files via upload

---
+++ 4_loops.py
@@ -0,0 +1,13 @@
+#math_Wahid_202310012
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10%3)	
+print(10//3)	#floor division 3
+print(10**3)	#exp10^3
+print(10==3)
+print(10!=3)
+print(10<3 or 3<10)
+print(10<3 and 3<10)
+print(not 10<3)
